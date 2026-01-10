bbb88cab8bac5d419a5272ecbcaee6492356dafa
Rakshith-Kotian <67151773+Rakshith-Kotian@users.noreply.github.com>
2026-01-10 12:34:32 +0530

    update import

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,6 @@
 declare module "*.css"; // Allow importing CSS files in TypeScript
+declare module "*.png"; // Allow importing PNG files in TypeScript
+declare module "*.jpg"; // Allow importing JPG files in TypeScript
+declare module "*.jpeg"; // Allow importing JPEG files in TypeScript
+declare module "*.gif"; // Allow importing GIF files in TypeScript
+declare module "*.svg"; // Allow importing SVG files in TypeScript
